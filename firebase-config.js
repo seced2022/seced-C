e6417165d4045941fdb1fcadd4159183b1c343a2
SECeD <114856253+seced2022@users.noreply.github.com>
2025-10-06 22:42:30 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhfv97WUcOfN0OJprE4P2qSD9jDYFtFyU",
+  authDomain: "secedc-6cd8a.firebaseapp.com",
+  projectId: "secedc-6cd8a",
+  storageBucket: "secedc-6cd8a.firebasestorage.app",
+  messagingSenderId: "425364282863",
+  appId: "1:425364282863:web:ca2a8f4cbe5ce3e55aaa1a"
+};
