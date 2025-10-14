e086a406795c85ac167a29f7f718302bd570ccfd
SECeD <114856253+seced2022@users.noreply.github.com>
2025-10-14 20:46:09 +0200

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,12 @@
-// Configuración de Firebase (rellena con tu proyecto real)
-window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
-  apiKey: "REEMPLAZA_AQUI",
-  authDomain: "REEMPLAZA_AQUI.firebaseapp.com",
-  projectId: "REEMPLAZA_AQUI",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDYRNhYISTXpHt_jK8dpT7aDUQskV_ZzuE",
+  authDomain: "secedcp.firebaseapp.com",
+  projectId: "secedcp",
+  storageBucket: "secedcp.firebasestorage.app",
+  messagingSenderId: "722200684437",
+  appId: "1:722200684437:web:f8486e13dd6ec0bdaaf89c",
+  measurementId: "G-XB0C93FM9W"
 };
 
 // Clave SOLO para abrir Auditoría
